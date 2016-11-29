9a4a2967668b67fac784ff4529b2c61f7ab170dd
Andres Dominguez <andres.dominguez.p@gmail.com>
2016-11-29 18:38:32 -0500

    add service to inject

--- src/com/karateca/injecttest/InjectTestAction.java
+++ src/com/karateca/injecttest/InjectTestAction.java
@@ -1,14 +1,18 @@
 package com.karateca.injecttest;
 
+import com.intellij.codeInsight.hint.HintManager;
 import com.intellij.lang.javascript.psi.JSCallExpression;
-import com.intellij.lang.javascript.psi.JSFunction;
-import com.intellij.lang.javascript.psi.JSFunctionExpression;
 import com.intellij.lang.javascript.psi.JSParameterList;
-import com.intellij.lang.javascript.psi.impl.JSFunctionExpressionImpl;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.Nullable;
 
@@ -16,20 +20,67 @@ public class InjectTestAction extends AnAction {
 
   @Override
   public void actionPerformed(AnActionEvent e) {
-    PsiElement element = e.getData(PlatformDataKeys.PSI_ELEMENT);
-
-    JSFunction parentOfType = PsiTreeUtil.getParentOfType(element, JSFunction.class);
-    JSFunctionExpression one = PsiTreeUtil.getParentOfType(element, JSFunctionExpression.class);
-    JSFunctionExpressionImpl teo = PsiTreeUtil.getParentOfType(element, JSFunctionExpressionImpl.class);
-
-    JSCallExpression injectElement = findInjectElement(element);
-    if (injectElement == null) {
+    Project project = getEventProject(e);
+    Editor editor = e.getData(PlatformDataKeys.EDITOR);
+    PsiFile file = e.getData(PlatformDataKeys.PSI_FILE);
+    if (project == null || editor == null || file == null) {
       return;
     }
 
+    final String selectedText = getSelectedText(e);
+    if (selectedText == null) {
+      showHint(editor, "Please select a string with the name of the service");
+      return;
+    }
+
+    PsiElement element = file.findElementAt(editor.getCaretModel().getOffset());
+    JSCallExpression injectElement = findInjectElement(element);
     JSParameterList parameterList = PsiTreeUtil.findChildOfType(injectElement, JSParameterList.class);
 
-    System.out.println(element);
+    if (parameterList == null) {
+      showHint(editor, "Can't find parameters in inject function");
+      return;
+    }
+
+    TextRange textRange = parameterList.getTextRange();
+    Document document = editor.getDocument();
+    String paramListString = document.getText(textRange);
+    final StringBuilder sb = new StringBuilder(paramListString);
+
+    // Does the para list has parens?
+    if (!paramListString.endsWith(")")) {
+      sb.insert(0, "(");
+      sb.append(")");
+    }
+
+    // Add comma if this is the second+ parameter.
+    if (parameterList.getChildren().length > 0) {
+      sb.insert(sb.length() - 1, ", ");
+    }
+
+    // Add the service with _name_;
+    sb.insert(sb.length() - 1, String.format("_%s_", selectedText));
+
+    CommandRunner.runCommand(project, () -> {
+      document.replaceString(textRange.getStartOffset(), textRange.getEndOffset(), sb.toString());
+    });
+  }
+
+  @Nullable
+  private String getSelectedText(AnActionEvent e) {
+    Caret caret = e.getData(PlatformDataKeys.CARET);
+    if (caret == null) {
+      return null;
+    }
+
+    if (caret.getSelectedText() == null || caret.getSelectedText().trim().length() == 0) {
+      return null;
+    }
+    return caret.getSelectedText().trim();
+  }
+
+  private void showHint(Editor editor, String s) {
+    HintManager.getInstance().showErrorHint(editor, s);
   }
 
   @Nullable
