14813272da9886b639122ad3075d473f3b497e43
Andres Dominguez <andres.dominguez.p@gmail.com>
2016-11-29 21:42:59 -0500

    Pull members up for common actions

--- src/com/karateca/injecttest/CommonAction.java
+++ src/com/karateca/injecttest/CommonAction.java
@@ -1,6 +1,25 @@
 package com.karateca.injecttest;
 
+import com.intellij.codeInsight.hint.HintManager;
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Editor;
+import org.jetbrains.annotations.Nullable;
 
 abstract class CommonAction extends AnAction {
+  @Nullable
+  String getSelectedText(Caret caret) {
+    if (caret == null) {
+      return null;
+    }
+
+    if (caret.getSelectedText() == null || caret.getSelectedText().trim().length() == 0) {
+      return null;
+    }
+    return caret.getSelectedText().trim();
+  }
+
+  void showHint(Editor editor, String s) {
+    HintManager.getInstance().showErrorHint(editor, s);
+  }
 }
