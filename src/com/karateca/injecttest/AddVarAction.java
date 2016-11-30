f553b6e7b4ab33607f5f0750cfc52debc1ba3324
Andres Dominguez <andres.dominguez.p@gmail.com>
2016-11-29 21:43:38 -0500

    extend commonAction

---
+++ src/com/karateca/injecttest/AddVarAction.java
@@ -0,0 +1,10 @@
+package com.karateca.injecttest;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class AddVarAction extends CommonAction {
+  @Override
+  public void actionPerformed(AnActionEvent anActionEvent) {
+
+  }
+}
